517e66db7ac1d48b1c718fe19bb6c9c5e4ab4d1f
kwaroran <kwaworan@gmail.com>
2023-05-07 23:34:11 +0900

    [fix] github stars shows on tauri

--- src/lib/Others/GithubStars.svelte
+++ src/lib/Others/GithubStars.svelte
@@ -6,6 +6,6 @@
     <script async defer src="https://buttons.github.io/buttons.js"></script>
 </svelte:head>
 <!-- Place this tag where you want the button to render. -->
-{#if isTauri}
+{#if !isTauri}
     <a class="github-button mt-4" href="https://github.com/kwaroran/risuAI" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" data-show-count="true" aria-label="Star kwaroran/risuAI on GitHub">Star</a>
 {/if}
