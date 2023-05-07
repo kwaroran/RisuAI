d6f441052424172f17f17d5705bf492fabb0ab29
kwaroran <kwaworan@gmail.com>
2023-05-07 16:30:20 +0900

    update version to 0.6.7

--- src/ts/database.ts
+++ src/ts/database.ts
@@ -6,7 +6,7 @@ import { saveImage as saveImageGlobal } from './globalApi';
 
 export const DataBase = writable({} as any as Database)
 export const loadedStore = writable(false)
-export let appVer = '0.6.6'
+export let appVer = '0.6.7'
 
 
 export function setDatabase(data:Database){
