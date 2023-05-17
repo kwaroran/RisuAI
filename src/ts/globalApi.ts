c805a81d97d6f35c587e37318a29afb3bc342b8d
kwaroran <kwaworan@gmail.com>
2023-05-17 15:24:50 +0900

    [fix] header applied to proxy

--- src/ts/globalApi.ts
+++ src/ts/globalApi.ts
@@ -369,9 +369,7 @@ export async function globalFetch(url:string, arg:{body?:any,headers?:{[key:stri
     
                 const da = await fetch(furl, {
                     body: JSON.stringify(arg.body),
-                    headers: {
-                        "risu-header": encodeURIComponent(JSON.stringify(arg.headers))
-                    },
+                    headers: arg.headers,
                     method: method
                 })
     
@@ -480,7 +478,9 @@ export async function globalFetch(url:string, arg:{body?:any,headers?:{[key:stri
                 
                     const da = await fetch(furl, {
                         body: JSON.stringify(arg.body),
-                        headers: arg.headers,
+                        headers: {
+                            "risu-header": encodeURIComponent(JSON.stringify(arg.headers))
+                        },
                         method: method
                     })
     
