1f7ce3c28d0c0fdb204ea15f0cf728861c5c073d
kwaroran <kwaworan@gmail.com>
2023-05-26 05:43:31 +0900

    bump version to 1.18.0

--- src/ts/database.ts
+++ src/ts/database.ts
@@ -7,7 +7,7 @@ import { cloneDeep } from 'lodash';
 
 export const DataBase = writable({} as any as Database)
 export const loadedStore = writable(false)
-export let appVer = '1.17.2'
+export let appVer = '1.18.0'
 
 
 export function setDatabase(data:Database){
