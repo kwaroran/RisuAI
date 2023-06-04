329de5d8603fe853b7f9e893010d1a7a169fe519
John He <johnhsq@126.com>
2023-06-04 16:23:15 +0800

    add support for zh-CN

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,6 +1,7 @@
 import { cloneDeep, merge } from "lodash";
 import { languageEnglish } from "./en";
 import { languageKorean } from "./ko";
+import { languageChinese } from "./cn";
 
 export let language:typeof languageEnglish = languageEnglish
 
@@ -8,6 +9,9 @@ export let language:typeof languageEnglish = languageEnglish
 export function changeLanguage(lang:string){
     if(lang === 'ko'){
         language = merge(cloneDeep(languageEnglish), languageKorean)
+    }
+    else if(lang === 'cn'){
+        language = merge(cloneDeep(languageEnglish), languageChinese)
     }
     else{
         language = languageEnglish
