7cd314e2a192f36c60bc2c6b443e8a90c4cb1cd7
kwaroran <kwaworan@gmail.com>
2023-06-24 16:06:00 +0900

    [ver] bump version to 1.27.4

--- src/ts/storage/database.ts
+++ src/ts/storage/database.ts
@@ -9,7 +9,7 @@ import { alertNormal } from '../alert';
 
 export const DataBase = writable({} as any as Database)
 export const loadedStore = writable(false)
-export let appVer = '1.27.3'
+export let appVer = '1.27.4'
 export let webAppSubVer = ''
 
 export function setDatabase(data:Database){
