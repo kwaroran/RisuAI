90420591962f5a3a2de4cafa9bbd1681bfeffafe
kwaroran <kwaworan@gmail.com>
2023-06-30 16:50:45 +0900

    [fix] file assets in sync

--- src/ts/storage/globalApi.ts
+++ src/ts/storage/globalApi.ts
@@ -87,7 +87,7 @@ export async function getFileSrc(loc:string) {
     }
     try {
 
-        if(forageStorage.isAccount){
+        if(forageStorage.isAccount && loc.startsWith('assets')){
             return hubURL + `/rs/` + loc
         }
         if(usingSw){
