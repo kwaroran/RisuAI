1152676d14d85928d42072a28d3c924b38c16ee5
kwaroran <kwaworan@gmail.com>
2023-07-02 20:20:09 +0900

    [feat] ignore google drive

--- src/lib/Setting/Pages/FilesSettings.svelte
+++ src/lib/Setting/Pages/FilesSettings.svelte
@@ -2,42 +2,47 @@
     import { language } from "src/lang";
     import { alertConfirm } from "src/ts/alert";
     import { checkDriver } from "src/ts/drive/drive";
+    import { DataBase } from "src/ts/storage/database";
     import { isNodeServer, isTauri } from "src/ts/storage/globalApi";
 
 </script>
 
 <h2 class="mb-2 text-2xl font-bold mt-2">{language.files}</h2>
-<button
-    on:click={async () => {
-        if(await alertConfirm(language.backupConfirm)){
-            localStorage.setItem('backup', 'save')
-            if(isTauri || isNodeServer){
-                checkDriver('savetauri')
-            }
-            else{
-                checkDriver('save')
-            }
-        }
-    }}
-    class="drop-shadow-lg p-3 border-borderc border-solid mt-2 flex justify-center items-center ml-2 mr-2 border-1 hover:bg-selected text-sm">
-    {language.savebackup}
-</button>
+{#if !$DataBase.account?.useSync}
 
-<button
-    on:click={async () => {
-        if((await alertConfirm(language.backupLoadConfirm)) && (await alertConfirm(language.backupLoadConfirm2))){
-            localStorage.setItem('backup', 'load')
-            if(isTauri || isNodeServer){
-                checkDriver('loadtauri')
+    <button
+        on:click={async () => {
+            if(await alertConfirm(language.backupConfirm)){
+                localStorage.setItem('backup', 'save')
+                if(isTauri || isNodeServer){
+                    checkDriver('savetauri')
+                }
+                else{
+                    checkDriver('save')
+                }
             }
-            else{
-                checkDriver('load')
+        }}
+        class="drop-shadow-lg p-3 border-borderc border-solid mt-2 flex justify-center items-center ml-2 mr-2 border-1 hover:bg-selected text-sm">
+        {language.savebackup}
+    </button>
+
+    <button
+        on:click={async () => {
+            if((await alertConfirm(language.backupLoadConfirm)) && (await alertConfirm(language.backupLoadConfirm2))){
+                localStorage.setItem('backup', 'load')
+                if(isTauri || isNodeServer){
+                    checkDriver('loadtauri')
+                }
+                else{
+                    checkDriver('load')
+                }
             }
-        }
-    }}
-    class="drop-shadow-lg p-3 border-borderc border-solid mt-2 flex justify-center items-center ml-2 mr-2 border-1 hover:bg-selected text-sm">
-    {language.loadbackup}
-</button>
+        }}
+        class="drop-shadow-lg p-3 border-borderc border-solid mt-2 flex justify-center items-center ml-2 mr-2 border-1 hover:bg-selected text-sm">
+        {language.loadbackup}
+    </button>
+
+{/if}
 
 <!-- <button
     on:click={async () => {
