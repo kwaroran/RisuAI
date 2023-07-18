5b5755bfcc83574fec9f091e99bd8be7e9751cfa
kwaroran <kwaworan@gmail.com>
2023-07-19 00:13:47 +0900

    [ver] 1.37.1

--- src/ts/storage/database.ts
+++ src/ts/storage/database.ts
@@ -9,7 +9,7 @@ import { alertNormal } from '../alert';
 
 export const DataBase = writable({} as any as Database)
 export const loadedStore = writable(false)
-export let appVer = '1.37.0'
+export let appVer = '1.37.1'
 export let webAppSubVer = ''
 
 export function setDatabase(data:Database){
