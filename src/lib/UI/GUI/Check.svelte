8d0e735be3e1f957c1c4c63f376c4604c283945d
kwaroran <kwaworan@gmail.com>
2023-07-20 22:41:52 +0900

    [fix] aria problem

--- src/lib/UI/GUI/Check.svelte
+++ src/lib/UI/GUI/Check.svelte
@@ -39,9 +39,10 @@
     />
     <span 
         class="w-5 h-5 min-w-5 min-h-5 rounded-md border-2 border-gray-600 flex justify-center items-center {check ? 'bg-borderc' : 'bg-gray-700'} transition-colors duration-200"
+        aria-describedby="{name}"
     >
         {#if check}
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="white" class="w-3 h-3">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="white" class="w-3 h-3" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/>
             </svg>
         {/if}
