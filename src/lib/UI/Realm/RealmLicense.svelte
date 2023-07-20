bfcf6766f0e72ca955a7f2dfd3c7e97b20ab83c7
kwaroran <kwaworan@gmail.com>
2023-07-20 16:21:41 +0900

    [feat] added disclamer

--- src/lib/UI/Realm/RealmLicense.svelte
+++ src/lib/UI/Realm/RealmLicense.svelte
@@ -8,7 +8,7 @@
 {#if Object.keys(CCLicenseData).includes(license)}
     <div class="w-full flex flex-row">
         <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <div class="flex flex-wrap flex-row gap-1 mt-2 items-center cursor-pointer" use:tooltip={CCLicenseData[license][1]} on:click|stopPropagation={() => {
+        <div class="flex flex-wrap flex-row gap-1 mt-2 items-center cursor-pointer" use:tooltip={CCLicenseData[license][1] + ". The License only applys to the text."} on:click|stopPropagation={() => {
             openURL(`https://creativecommons.org/licenses/${CCLicenseData[license][0]}/4.0/`)
         }}>
             <img alt="creative commons" class="cc" src="https://i.creativecommons.org/l/{CCLicenseData[license][0]}/4.0/88x31.png" />
