6e2f5a471dd4ba06a7a6a4ff9f607789e735df05
kwaroran <kwaworan@gmail.com>
2023-07-21 18:05:50 +0900

    [feat] added translation guide

--- src/lib/Setting/Pages/LanguageSettings.svelte
+++ src/lib/Setting/Pages/LanguageSettings.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
     import Check from "src/lib/UI/GUI/Check.svelte";
-import { changeLanguage, language } from "src/lang";
+    import { changeLanguage, language } from "src/lang";
     import { DataBase } from "src/ts/storage/database";
     import { sleep } from "src/ts/util";
     import Help from "src/lib/Others/Help.svelte";
-  import OptionInput from "src/lib/UI/GUI/OptionInput.svelte";
-  import SelectInput from "src/lib/UI/GUI/SelectInput.svelte";
+    import OptionInput from "src/lib/UI/GUI/OptionInput.svelte";
+    import SelectInput from "src/lib/UI/GUI/SelectInput.svelte";
+    import { alertNormal } from "src/ts/alert";
+    import { downloadFile } from "src/ts/storage/globalApi";
+    import { languageEnglish } from "src/lang/en";
     let langChanged = false
 </script>
 <h2 class="mb-2 text-2xl font-bold mt-2">{language.language}</h2>
 
 <span class="text-neutral-200 mt-4">{language.UiLanguage}</span>
 <SelectInput className="mt-2" bind:value={$DataBase.language} on:change={async () => {
+    if($DataBase.language === 'translang'){
+        downloadFile('lang.json', Buffer.from(JSON.stringify(languageEnglish, null, 4), 'utf-8'))
+        alertNormal("Downloaded JSON, translate it, and send it to the dev by discord DM and email. I will add it to the next version.")
+        $DataBase.language = 'en'
+    }
     await sleep(10)
     changeLanguage($DataBase.language)
     langChanged = true
@@ -19,6 +27,7 @@ import { changeLanguage, language } from "src/lang";
     <OptionInput value="en" >English</OptionInput>
     <OptionInput value="ko" >한국어</OptionInput>
     <OptionInput value="cn" >中文</OptionInput>
+    <OptionInput value="translang" >[Translate in your own language]</OptionInput>
 </SelectInput>
 {#if langChanged}
     <span class="bg-red-500 text-sm">Close the settings to take effect</span>
