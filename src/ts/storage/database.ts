dbf3d89e4494c0fa43da57cc037e2e417808a898
kwaroran <kwaworan@gmail.com>
2023-07-26 07:21:41 +0900

    [ver] 1.43.1

--- src/ts/storage/database.ts
+++ src/ts/storage/database.ts
@@ -9,7 +9,7 @@ import { alertNormal } from '../alert';
 
 export const DataBase = writable({} as any as Database)
 export const loadedStore = writable(false)
-export let appVer = '1.43.0'
+export let appVer = '1.43.1'
 export let webAppSubVer = ''
 
 export function setDatabase(data:Database){
