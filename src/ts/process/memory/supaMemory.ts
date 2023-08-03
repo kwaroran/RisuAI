a6204c0e6ad9830ce110a2608c684dff84865289
kwaroran <kwaworan@gmail.com>
2023-08-03 09:20:46 +0900

    [fix] hypamemory

--- src/ts/process/memory/supaMemory.ts
+++ src/ts/process/memory/supaMemory.ts
@@ -205,6 +205,7 @@ export async function supaMemory(
             const hypa = new HypaProcesser()
             hypa.oaikey = db.supaMemoryKey
             hypa.vectors = []
+            hypaChunks = hypaChunks.filter((value) => value.length > 1)
             await hypa.addText(hypaChunks.filter((value, index, self) => {
                 return self.indexOf(value) === index;
             }))
