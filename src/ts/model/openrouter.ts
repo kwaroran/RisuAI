2f1134edc2cc88a3d2101c04b214f27bec90ac13
kwaroran <kwaworan@gmail.com>
2023-10-26 06:22:58 +0900

    [fix] openrouter

--- src/ts/model/openrouter.ts
+++ src/ts/model/openrouter.ts
@@ -1,6 +1,17 @@
+import { get } from "svelte/store"
+import { DataBase } from "../storage/database"
+
 export async function openRouterModels() {
     try {
-        const aim = fetch("https://openrouter.ai/api/v1/models")  
+        const db = get(DataBase)
+        let headers = {
+            "Authorization": "Bearer " + db.openrouterKey,
+            "Content-Type": "application/json"
+        }
+
+        const aim = fetch("https://openrouter.ai/api/v1/models", {
+            headers: headers
+        })  
         const res = await (await aim).json()
         return res.data.map((v:any) => {
             return v.id
