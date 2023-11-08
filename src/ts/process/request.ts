cd322a5b1f7183942af9106742cd8901a12b5769
kwaroran <kwaworan@gmail.com>
2023-11-08 16:35:32 +0900

    [fix] seed

--- src/ts/process/request.ts
+++ src/ts/process/request.ts
@@ -206,12 +206,11 @@ export async function requestChatDataMain(arg:requestDataArgument, model:'model'
                 frequency_penalty: arg.frequencyPenalty || (db.frequencyPenalty / 100),
                 logit_bias: bias,
                 stream: false,
-                seed: db.generationSeed || -1
             })
 
-            body.seed = db.generationSeed
-            if(body.seed === -1 || (!body.seed) || (body.seed < 0)){
-                delete body.seed
+            if(db.generationSeed > 0){
+                // @ts-ignore
+                body.seed = db.generationSeed
             }
 
             let replacerURL = aiModel === 'openrouter' ? "https://openrouter.ai/api/v1/chat/completions" :
