705423ee26713c453fa6083708e49ebba3b21d8d
kwaroran <kwaworan@gmail.com>
2023-11-13 00:14:17 +0900

    [ver] 1.57.3

--- src/ts/storage/database.ts
+++ src/ts/storage/database.ts
@@ -14,7 +14,7 @@ import type { Proompt } from '../process/proompt';
 
 export const DataBase = writable({} as any as Database)
 export const loadedStore = writable(false)
-export let appVer = "1.57.2"
+export let appVer = "1.57.3"
 export let webAppSubVer = ''
 
 export function setDatabase(data:Database){
