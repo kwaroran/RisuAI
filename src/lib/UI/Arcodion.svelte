d71ef1b8a2eb43ddd7d6c54d24b133d87683f8eb
kwaroran <kwaworan@gmail.com>
2023-11-27 19:11:48 +0900

    [feat] better otherbots screen

--- src/lib/UI/Arcodion.svelte
+++ src/lib/UI/Arcodion.svelte
@@ -1,15 +1,33 @@
 <script lang="ts">
     export let name = ""
     let open = false
+    export let styled = false
 </script>
 
-<div class="flex flex-col">
-    <button class="hover:bg-selected px-6 py-2 text-lg" class:bg-selected={open} on:click={() => {
-        open = !open
-    }}>{name}</button>
-    {#if open}
-        <div class="flex flex-col bg-darkbg">
-            <slot></slot>
-        </div>
-    {/if}
-</div>
+{#if styled}
+    <div class="flex flex-col mt-2">
+        <button class="hover:bg-selected px-6 py-2 text-lg rounded-t-md border-selected border"
+            class:bg-selected={open}
+            class:rounded-b-md={!open}
+            on:click={() => {
+                open = !open
+            }}
+        >{name}</button>
+        {#if open}
+            <div class="flex flex-col border border-selected p-2 rounded-b-md">
+                <slot></slot>
+            </div>
+        {/if}
+    </div>
+{:else}
+    <div class="flex flex-col">
+        <button class="hover:bg-selected px-6 py-2 text-lg" class:bg-selected={open} on:click={() => {
+            open = !open
+        }}>{name}</button>
+        {#if open}
+            <div class="flex flex-col bg-darkbg">
+                <slot></slot>
+            </div>
+        {/if}
+    </div>
+{/if}
