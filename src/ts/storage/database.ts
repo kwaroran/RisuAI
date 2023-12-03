bb376f231f92255698eef5741229c34e8e4a85ec
kwaroran <kwaworan@gmail.com>
2023-12-03 22:42:24 +0900

    [ver] 1.63.3

--- src/ts/storage/database.ts
+++ src/ts/storage/database.ts
@@ -15,7 +15,7 @@ import type { OobaChatCompletionRequestParams } from '../model/ooba';
 
 export const DataBase = writable({} as any as Database)
 export const loadedStore = writable(false)
-export let appVer = "1.63.2"
+export let appVer = "1.63.3"
 export let webAppSubVer = ''
 
 export function setDatabase(data:Database){
