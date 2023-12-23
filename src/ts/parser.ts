d3fed78c0cf6bcc48a794c77282851cb7d0c9d8b
kwaroran <kwaworan@gmail.com>
2023-12-23 17:12:09 +0900

    [feat] increase limit more

--- src/ts/parser.ts
+++ src/ts/parser.ts
@@ -378,7 +378,7 @@ type matcherArg = {
     consistantChar?:boolean
 }
 const matcher = (p1:string,matcherArg:matcherArg) => {
-    if(p1.length > 30000){
+    if(p1.length > 100000){
         return ''
     }
     const lowerCased = p1.toLocaleLowerCase()
