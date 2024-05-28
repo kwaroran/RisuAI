ca8a5009c0c5b0643c637f0a6b842720704c55ba
kwaroran <amasio1234@proton.me>
2024-05-28 20:46:11 +0900

    chore: Update patchnote version to 1.108.0

--- src/etc/patchNote.ts
+++ src/etc/patchNote.ts
@@ -1,16 +1,16 @@
 export const patchNote = {
-    version: "1.107",
+    version: "1.108",
     content: 
 `
-# Update 1.107 (Things & Stuff Update)
-- Added custom toggle system in prompt template
-- Added default variable setting in prompt template and characters
-- Removed experimental flag from HypaMemory V2
-- Added HypaMemory V2 chunk size and allocation size setting
-- Changed old COT toggle to deprecated, dude to new custom toggle system
-- Fixed Supamemory summary not working when generation choices are more than 1
-- Fixed triggerscript not handling undefined values as null
-- Fixed Character Card V3 export not working or missing fields in specific cases
+# Update 1.108 (Low Level Access)
+- Added new character field: Low Level Access
+   - Enabling this field will allow you to access the low level functions, like accessing the AI model via triggers
+   - Added Run Main Model trigger
+   - Added Resend AI trigger
+   - Added Check Similarity trigger
+   - Added Show Alert trigger
+- Added is Truthy condition in trigger
+- Fixed lorebook not threatening as a match if the key is not lowercase
 `
 }
 
