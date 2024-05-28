01d43716aed6ba7106f95a5ba48dc4d4581c9376
kwaroran <amasio1234@proton.me>
2024-05-28 20:43:13 +0900

    chore: Update version to 1.108.0

--- src/ts/storage/database.ts
+++ src/ts/storage/database.ts
@@ -14,7 +14,7 @@ import type { OobaChatCompletionRequestParams } from '../model/ooba';
 
 export const DataBase = writable({} as any as Database)
 export const loadedStore = writable(false)
-export let appVer = "1.107.4"
+export let appVer = "1.108.0"
 export let webAppSubVer = ''
 
 export function setDatabase(data:Database){
