2d6ba809388fc1243dc76aaf265e62754267d051
kwaroran <amasio1234@proton.me>
2024-06-10 15:53:52 +0900

    Remove unnecessary imports from MainMenu.svelte

--- src/lib/UI/MainMenu.svelte
+++ src/lib/UI/MainMenu.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
     import { DataBase, appVer, webAppSubVer } from "src/ts/storage/database";
-    import GithubStars from "../Others/GithubStars.svelte";
     import Hub from "./Realm/RealmMain.svelte";
     import { OpenRealmStore } from "src/ts/stores";
     import { ArrowLeft } from "lucide-svelte";
-    import { isNodeServer, isTauri, openURL } from "src/ts/storage/globalApi";
+    import { isNodeServer, isTauri } from "src/ts/storage/globalApi";
     import { language } from "src/lang";
     import { getRisuHub } from "src/ts/characterCards";
     import RisuHubIcon from "./Realm/RealmHubIcon.svelte";
     import Title from "./Title.svelte";
-    import { getPatchNote } from "src/etc/patchNote";
 
 </script>
 <div class="h-full w-full flex flex-col overflow-y-auto items-center">
