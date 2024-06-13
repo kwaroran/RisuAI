9d1e2bfb28f21600aa8be0bf507b3ab8c1aab527
kwaroran <amasio1234@proton.me>
2024-06-14 08:15:48 +0900

    feat: add PlaygroundParser component to PlaygroundMenu

---
+++ src/lib/Playground/PlaygroundParser.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { ParseMarkdown } from "src/ts/parser";
+    import TextAreaInput from "../UI/GUI/TextAreaInput.svelte";
+    let input = "";
+    let output = "";
+    const onInput = async () => {
+        try {
+            output = await ParseMarkdown(input)
+        } catch (e) {
+            output = `Error: ${e}`
+        }
+    }
+</script>
+
+<h2 class="text-4xl text-textcolor my-6 font-black relative">Full Parser</h2>
+
+<span class="text-textcolor text-lg">Input</span>
+
+<TextAreaInput onInput={onInput} bind:value={input} optimaizedInput={false} />
+
+<span class="text-textcolor text-lg">Output HTML</span>
+
+<TextAreaInput value={output} />
