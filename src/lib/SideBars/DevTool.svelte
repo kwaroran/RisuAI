721fda615143f8909a92e2fd68b4050b7dfbcf56
kwaroran <amasio1234@proton.me>
2024-08-24 19:54:10 +0900

    Add request log in devtool

--- src/lib/SideBars/DevTool.svelte
+++ src/lib/SideBars/DevTool.svelte
@@ -2,24 +2,33 @@
     import { CurrentChat } from "src/ts/stores";
   import TextInput from "../UI/GUI/TextInput.svelte";
   import NumberInput from "../UI/GUI/NumberInput.svelte";
+  import Button from "../UI/GUI/Button.svelte";
+  import { getRequestLog } from "src/ts/storage/globalApi";
+  import { alertMd } from "src/ts/alert";
+  import Arcodion from "../UI/Arcodion.svelte";
 
     
 </script>
 
-<span>Variables</span>
-<div class="rounded-md border border-darkborderc grid grid-cols-2 gap-2 p-2">
-    {#if $CurrentChat.scriptstate &&  Object.keys($CurrentChat.scriptstate).length > 0}
-        {#each Object.keys($CurrentChat.scriptstate) as key}
-            <span>{key}</span>
-            {#if typeof $CurrentChat.scriptstate[key] === "object"}
-                <div class="p-2 text-center">Object</div>
-            {:else if typeof $CurrentChat.scriptstate[key] === "string"}
-                <TextInput bind:value={$CurrentChat.scriptstate[key]} />
-            {:else if typeof $CurrentChat.scriptstate[key] === "number"}
-                <NumberInput bind:value={$CurrentChat.scriptstate[key]} />
-            {/if}
-        {/each}
-    {:else}
-        <div class="p-2 text-center">No variables</div>
-    {/if}
-</div>
+<Arcodion styled name={"Variables"}>
+    <div class="rounded-md border border-darkborderc grid grid-cols-2 gap-2 p-2">
+        {#if $CurrentChat.scriptstate &&  Object.keys($CurrentChat.scriptstate).length > 0}
+            {#each Object.keys($CurrentChat.scriptstate) as key}
+                <span>{key}</span>
+                {#if typeof $CurrentChat.scriptstate[key] === "object"}
+                    <div class="p-2 text-center">Object</div>
+                {:else if typeof $CurrentChat.scriptstate[key] === "string"}
+                    <TextInput bind:value={$CurrentChat.scriptstate[key]} />
+                {:else if typeof $CurrentChat.scriptstate[key] === "number"}
+                    <NumberInput bind:value={$CurrentChat.scriptstate[key]} />
+                {/if}
+            {/each}
+        {:else}
+            <div class="p-2 text-center">No variables</div>
+        {/if}
+    </div>
+</Arcodion>
+
+<Button on:click={() => {
+    alertMd(getRequestLog())
+}}>Request Log</Button>
