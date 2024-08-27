2ae5106453142997ccbe270cfb60ed71802fde66
kwaroran <amasio1234@proton.me>
2024-08-27 22:03:17 +0900

    Update version to 126.0.0

--- src/ts/storage/database.ts
+++ src/ts/storage/database.ts
@@ -1,6 +1,6 @@
 export const DataBase = writable({} as any as Database)
 export const loadedStore = writable(false)
-export let appVer = "125.0.1"
+export let appVer = "126.0.0"
 export let webAppSubVer = ''
 
 import { get, writable } from 'svelte/store';
