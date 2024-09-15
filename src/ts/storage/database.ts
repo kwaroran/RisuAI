e8c482e5e62384984f9926829d50e4ac55c7fd8a
kwaroran <amasio1234@proton.me>
2024-09-15 18:01:36 +0900

    Update version to 131.2.0

--- src/ts/storage/database.ts
+++ src/ts/storage/database.ts
@@ -1,6 +1,6 @@
 export const DataBase = writable({} as any as Database)
 export const loadedStore = writable(false)
-export let appVer = "131.1.0"
+export let appVer = "131.2.0"
 export let webAppSubVer = ''
 
 import { get, writable } from 'svelte/store';
