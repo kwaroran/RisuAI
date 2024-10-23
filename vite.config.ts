f0782928435dd12b7157db6cfc6bc55e2bad3b78
kwaroran <amasio1234@proton.me>
2024-10-23 18:36:28 +0900

    Refactor svelte-preprocess import in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import sveltePreprocess from "svelte-preprocess";
+import {sveltePreprocess} from "svelte-preprocess";
 import wasm from "vite-plugin-wasm";
 import { internalIpV4 } from 'internal-ip'
 import topLevelAwait from "vite-plugin-top-level-await";
