7e1fa69b565cf16cdd1bf6ac4556dadc07878c2f
kwaroran <amasio1234@proton.me>
2024-11-30 02:20:01 +0900

    Bump version to 140.1.0

--- src/ts/storage/database.svelte.ts
+++ src/ts/storage/database.svelte.ts
@@ -12,7 +12,7 @@ import { defaultColorScheme, type ColorScheme } from '../gui/colorscheme';
 import type { PromptItem, PromptSettings } from '../process/prompt';
 import type { OobaChatCompletionRequestParams } from '../model/ooba';
 
-export let appVer = "140.0.3"
+export let appVer = "140.1.0"
 export let webAppSubVer = ''
 
 
