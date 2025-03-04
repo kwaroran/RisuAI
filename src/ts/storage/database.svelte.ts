5026a96ce99444843fd2a23a101ce1cdded25f75
Kwaroran <amasio1234@proton.me>
2025-03-05 05:42:20 +0900

    chore: update application version to 151.0.0

--- src/ts/storage/database.svelte.ts
+++ src/ts/storage/database.svelte.ts
@@ -12,7 +12,7 @@ import { defaultColorScheme, type ColorScheme } from '../gui/colorscheme';
 import type { PromptItem, PromptSettings } from '../process/prompt';
 import type { OobaChatCompletionRequestParams } from '../model/ooba';
 
-export let appVer = "150.4.1"
+export let appVer = "151.0.0"
 export let webAppSubVer = ''
 
 
