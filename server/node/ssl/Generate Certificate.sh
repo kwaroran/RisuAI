1a2e44f9ae1b516cffff06cccaba62b9b4435c66
shirosaki-hana <usagi@kirimasyaro.net>
2025-03-20 16:38:53 +0900

    Update Generate Certificate.sh

--- server/node/ssl/Generate Certificate.sh
+++ server/node/ssl/Generate Certificate.sh
@@ -4,5 +4,5 @@ openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout certificate/ca.key 
 openssl req -new -nodes -newkey rsa:2048 -keyout certificate/server.key -out certificate/server.csr -config server.conf
 openssl x509 -req -in certificate/server.csr -CA certificate/ca.crt -CAkey certificate/ca.key -CAcreateserial -out certificate/server.crt -days 3650 -extensions req_ext -extfile server.conf
 
-chmod 600 certificate/ca.key certificate/server.key
+chmod 644 certificate/ca.key certificate/server.key
 chmod 644 certificate/ca.crt certificate/server.crt certificate/server.csr
