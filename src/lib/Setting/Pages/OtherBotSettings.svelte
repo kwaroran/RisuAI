5d57f491a645659ce93d6850be767242c4363c22
Joseph Kang <kangjoseph90@gmail.com>
2025-06-02 15:54:54 +0900

    Revert "Update OtherBotSettings.svelte"
    This reverts commit 9306d9cc78b4020ce92c361d6877fdf57eb2f7b2.

--- src/lib/Setting/Pages/OtherBotSettings.svelte
+++ src/lib/Setting/Pages/OtherBotSettings.svelte
@@ -60,8 +60,6 @@
             reference_strength_multiple: [0.7],
             vibe_data: undefined,
             vibe_model_selection: undefined,
-            use_char_uc: true,
-            use_multi_character_prompt: true,
         }
         if (DBState.db.NAIImgConfig.sampler === 'ddim_v3'){
             DBState.db.NAIImgConfig.sm = false
@@ -292,13 +290,14 @@
             <Check bind:check={DBState.db.NAII2I} name="Enable I2I"/>
 
             {#if DBState.db.NAIImgModel === 'nai-diffusion-4-full'
-            || DBState.db.NAIImgModel === 'nai-diffusion-4-curated-preview'
-            || DBState.db.NAIImgModel === 'nai-diffusion-4-5-full'
-            || DBState.db.NAIImgModel === 'nai-diffusion-4-5-curated'}
+            || DBState.db.NAIImgModel === 'nai-diffusion-4-curated-preview'}
                 <Check bind:check={DBState.db.NAIImgConfig.autoSmea} name='Auto Smea'/>
+                <Check bind:check={DBState.db.NAIImgConfig.use_coords} name='Use coords'/>
                 <Check bind:check={DBState.db.NAIImgConfig.legacy_uc} name='Use legacy uc'/>
-                <Check bind:check={DBState.db.NAIImgConfig.use_multi_character_prompt} name='Use Multi Character Prompt'/>
-                <Check bind:check={DBState.db.NAIImgConfig.use_char_uc} name='Use auto character uc'/>
+
+                <Check bind:check={DBState.db.NAIImgConfig.v4_prompt.use_coords} name='Use v4 prompt coords'/>
+                <Check bind:check={DBState.db.NAIImgConfig.v4_prompt.use_order} name='Use v4 prompt order'/>
+                <Check bind:check={DBState.db.NAIImgConfig.v4_negative_prompt.legacy_uc} name='Use v4 negative prompt legacy uc'/>
             {/if}
 
             {#if DBState.db.NAII2I}
