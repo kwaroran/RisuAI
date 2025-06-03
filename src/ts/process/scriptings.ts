ad68005f21fc7726a1b4d8308db0e9102f8bec46
Kwaroran <amasio1234@proton.me>
2025-06-03 17:22:50 +0900

    PoC python, not tested & no error handling yet

--- src/ts/process/scriptings.ts
+++ src/ts/process/scriptings.ts
@@ -37,7 +37,7 @@ interface LuaScriptingEngineState extends BasicScriptingEngineState {
 }
 
 interface PythonScriptingEngineState extends BasicScriptingEngineState {
-    
+    pyodide?: PyodideContext
     type: 'py';
 }
 
@@ -84,13 +84,12 @@ export async function runScripted(code:string, arg:{
                 ScriptingEngineState.code = code
                 ScriptingEngineState.engine = await luaFactory.createEngine({injectObjects: true})
                 const luaEngine = ScriptingEngineState.engine
-                declareAPI = (name: string, func: Function) => {
-                    luaEngine.global.set(name, func)
-                }
+                declareAPI = luaEngine.global.set
             }
             if(ScriptingEngineState.type === 'py'){
-
-                
+                ScriptingEngineState.pyodide?.close()
+                ScriptingEngineState.pyodide = new PyodideContext()
+                declareAPI = ScriptingEngineState.pyodide.declareAPI
             }
             declareAPI('getChatVar', (id:string,key:string) => {
                 return ScriptingEngineState.getVar(key)
@@ -661,6 +660,9 @@ export async function runScripted(code:string, arg:{
             if(ScriptingEngineState.type === 'lua'){
                 await ScriptingEngineState.engine?.doString(luaCodeWarper(code))
             }
+            if(ScriptingEngineState.type === 'py'){
+                await ScriptingEngineState.pyodide?.init(code)
+            }
             ScriptingEngineState.code = code
         }
         let accessKey = v4()
@@ -730,6 +732,38 @@ export async function runScripted(code:string, arg:{
                 }
             } catch (error) {
                 console.error(error)
+            }
+        }
+        if(ScriptingEngineState.type === 'py'){
+            switch(mode){
+                case 'input':{
+                    res = await ScriptingEngineState.pyodide?.python(`onInput('${accessKey}')`)
+                    break
+                }
+                case 'output':{
+                    res = await ScriptingEngineState.pyodide?.python(`onOutput('${accessKey}')`)
+                    break
+                }
+                case 'start':{
+                    res = await ScriptingEngineState.pyodide?.python(`onStart('${accessKey}')`)
+                    break
+                }
+                case 'onButtonClick':{
+                    res = await ScriptingEngineState.pyodide?.python(`onButtonClick('${accessKey}', '${data}')`)
+                    break
+                }
+                case 'editRequest':
+                case 'editDisplay':
+                case 'editInput':
+                case 'editOutput':{
+                    res = await ScriptingEngineState.pyodide?.python(`callListenMain('${mode}', '${accessKey}', '${JSON.stringify(data)}')`)
+                    res = JSON.parse(res)
+                    break
+                }
+                default:{
+                    res = await ScriptingEngineState.pyodide?.python(`${mode}('${accessKey}')`)
+                    break
+                }
             }
         }
         ScriptingSafeIds.delete(accessKey)
@@ -1017,4 +1051,94 @@ export async function runLuaButtonTrigger(char:character|groupChat|simpleCharact
         throw(error)
     }
     return runResult   
+}
+
+class PyodideContext{
+    worker: Worker;
+    apis: Record<string, (...args:any[]) => any> = {};
+    inited: boolean = false;
+    constructor(){
+        this.worker = new Worker(new URL('./pyworker.js', import.meta.url), {
+            type: 'module'
+        })
+        this.worker.onmessage = (event:MessageEvent) => {
+            if(event.data.type === 'call'){
+                const { function: func, args, callId } = event.data;
+                if(this.apis[func]){
+                    this.apis[func](...args).then((result) => {
+                        this.worker.postMessage({
+                            type: 'functionResult',
+                            callId: callId,
+                            result: result
+                        });
+                    }).catch((error) => {
+                        this.worker.postMessage({
+                            type: 'error',
+                            error: error.message,
+                            id: callId
+                        });
+                    });
+                } else {
+                    this.worker.postMessage({
+                        type: 'error',
+                        error: `Function ${func} not found`,
+                        id: callId
+                    });
+                }
+            }
+        }
+    }
+    async declareAPI(name:string, func:(...args:any[]) => any){
+        this.apis[name] = func;
+    }
+    async init(code:string){
+        if(this.inited){
+            return;
+        }
+        const id = crypto.randomUUID();
+        return new Promise<void>((resolve, reject) => {
+            this.worker.onmessage = (event:MessageEvent) => {
+                if(event.data.id !== id){
+                    return
+                }
+
+                if(event.data.type === 'init'){
+                    this.inited = true;
+                    resolve();
+                } else if(event.data.type === 'error'){
+                    reject(new Error(event.data.error));
+                }
+            };
+            this.worker.postMessage({
+                type: 'init',
+                code: code,
+                id: id,
+                moduleFunctions: Object.keys(this.apis)
+            });
+        });
+    }
+    async python(call:string){
+        const id = crypto.randomUUID();
+        return new Promise<any>((resolve, reject) => {
+            this.worker.onmessage = (event:MessageEvent) => {
+                if(event.data.id !== id){
+                    return
+                }
+
+                if(event.data.type === 'python'){
+                    resolve(event.data.call);
+                } else if(event.data.type === 'error'){
+                    reject(new Error(event.data.error));
+                }
+            };
+            this.worker.postMessage({
+                type: 'python',
+                call: call,
+                id: id
+            });
+        });
+    }
+    async close(){
+        this.worker.terminate();
+    }
 }
