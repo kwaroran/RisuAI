307cb9617371f6fb9190b29af874cca35f08484d
cubicj <jongjin6038@gmail.com>
2025-07-23 12:08:30 +0900

    default add trigger type change

--- src/lib/SideBars/Scripts/TriggerList2.svelte
+++ src/lib/SideBars/Scripts/TriggerList2.svelte
@@ -2001,7 +2001,7 @@
                         <button class="p-2 border-t-darkborderc text-start text-textcolor2 hover:text-textcolor focus:bg-bgcolor" onclick={() => {
                             value.push({
                                 comment: "",
-                                type: "start",
+                                type: "manual",
                                 conditions: [],
                                 effect: []
                             })
