e57a2c4879b3ad6f13ec3502ab385a85c3256cd5
kwaroran <amasio1234@proton.me>
2025-12-26 21:28:23 +0900

    fix: update paths in postbuild script for static file copying

--- server/hono/src/utils/postbuild.js
+++ server/hono/src/utils/postbuild.js
@@ -2,5 +2,5 @@ import fs from 'fs'
 
 //this is expected to run in root dir
 
-fs.cpSync('./dist', 'server/hono/src/static', { recursive: true })
-fs.cpSync('./dist', 'server/hono/src/.vercel/output/static', { recursive: true })
+fs.cpSync('./dist', 'server/hono/static', { recursive: true })
+fs.cpSync('./dist', 'server/hono/.vercel/output/static', { recursive: true })
